40d453772e561ac9ea1c64629785d61b1851b2e0
JayaneKarla <jks8@aluno.ifal.edu.br>
2024-12-09 20:43:01 -0300

    Create q.7.js

---
+++ q.7.js
@@ -0,0 +1,12 @@
+let somaDasIdades = 0;
+
+for (let i = 1; i <= 20; i++) {
+
+    let idade = parseInt(prompt("Digite a idade da " + i + "º" + " pessoa:"));
+    
+    somaDasIdades+= idade;
+}
+
+let mediaDasIdades = somaDasIdades/20;
+
+alert("A soma de todas as idades é igual a : " + mediaDasIdades);
